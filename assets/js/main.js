d3f921e3a9de0e4a0077529d1ace7739d437410f
eduardordl97 <erdl5259@gmail.com>
2021-06-05 22:15:21 -0500

    Correcciones y modo oscuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,26 @@ const showMenu = (toggleId,navId) =>{
     }
 }
 showMenu('nav-toggle','nav-menu')
+
+
+
+
+function change(){
+    if(document.getElementsByClassName('bx-moon')){
+        document.getElementById('change-theme').classList.remove('bx-moon');
+        document.getElementById('change-theme').classList.add('bx-sun');
+    }else if(document.getElementsByClassName('bx-sun')){
+        document.getElementById('change-theme').classList.remove('bx-sun');
+        document.getElementById('change-theme').classList.add('bx-moon');
+    }
+   
+     
+
+}
+document.getElementById('selector-theme').onclick = function(){
+    change();
+}
+
 /*----------Activar y remover menu-------------*/
 const navLink = document.querySelectorAll('.nav_link')
 function linkAction(){
@@ -23,6 +43,42 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click',linkAction))
 /*----------Mostrar Menu-------------*/
+$(document).ready(function(){
+    var theme = $('#theme')  
+        
+    $('#change-theme').click(function(){
+        if(localStorage.getItem('light-mode')=== 'true'){
+            theme.attr('href',"./assets/css/styles-DM.css") 
+                if(theme.attr('href',"./assets/css/styles-DM.css")){ 
+                    localStorage.setItem('light-mode','false') 
+                    localStorage.setItem('dark-mode','true') 
+                }else{
+                    localStorage.setItem('light-mode','true') 
+                    localStorage.setItem('dark-mode','false') 
+                }
+
+        }else{
+            theme.attr('href',"./assets/css/styles.css")
+                if(theme.attr('href',"./assets/css/styles.css")){ 
+                    localStorage.setItem('light-mode','true') 
+                    localStorage.setItem('dark-mode','false') 
+                }else{
+                    localStorage.setItem('light-mode','false') 
+                    localStorage.setItem('dark-mode','true') 
+                }
+        }
+            
+            
+    })
+      
+    if(localStorage.getItem('light-mode')=== 'true'){
+        theme.attr('href','./assets/css/styles.css') 
+        
+    }else if(localStorage.getItem('dark-mode')=== 'true'){
+        theme.attr('href','./assets/css/styles-DM.css')
+    }
+    
+})
 /*----------Mostrar Menu-------------*/
 /*----------Mostrar Menu-------------*/
 /*----------Mostrar Menu-------------*/
