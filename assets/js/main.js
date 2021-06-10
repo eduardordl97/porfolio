de91d76fcf15eb24e07a145268e5b7654797d516
eduardordl97 <erdl5259@gmail.com>
2021-06-09 23:29:30 -0500

    Se añade from funcional y urls

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,4 +133,26 @@ sr.reveal('.porfolio_container',{});
 sr.reveal('.contact__input',{interval: 200}); 
 sr.reveal('.contact__form',{}); 
 
+//FORM
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'aplication/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('Gracias por contactarme')
+    }
+}
+
 
